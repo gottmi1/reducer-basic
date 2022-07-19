89fa100cd3daa518016042d1da5b84c14a25e959
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-07-20 05:51:30 +0900

    useSelector, useDispatch를 사용한 리팩토링

--- src/containers/TodoFormContainer.js
+++ src/containers/TodoFormContainer.js
@@ -1,14 +1,26 @@
-import { connect } from "react-redux";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
 import TodoForm from "../components/TodoForm";
 import { addTodo } from "../redux/actions";
 
-const TodoFormContiner = connect(
-  (state) => ({}),
-  (dispatch) => ({
-    add: (text) => {
+// const TodoFormContiner = connect(
+//   (state) => ({}),
+//   (dispatch) => ({
+//     add: (text) => {
+//       dispatch(addTodo(text));
+//     },
+//   })
+// )(TodoForm);
+
+export default function TodoFormContiner() {
+  const dispatch = useDispatch();
+
+  const add = useCallback(
+    (text) => {
       dispatch(addTodo(text));
     },
-  })
-)(TodoForm);
+    [dispatch]
+  );
 
-export default TodoFormContiner;
+  return <TodoForm add={add} />;
+}
--- src/containers/TodoListContainer.js
+++ src/containers/TodoListContainer.js
@@ -1,4 +1,4 @@
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 import TodoList from "../components/TodoList";
 
 const mapStateToProps = (state) => {
@@ -10,11 +10,17 @@ const mapDispatchToProps = (dispatch) => {
   return {};
 };
 
-const TodoListContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoList);
+// const TodoListContainer = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(TodoList);
 
 // 초기 형태 connect(config)(TodoList), connect()로 만든 함수를 두번째 ()로 바로 실행
+
+function TodoListContainer() {
+  // 얘가 하는일 : 컴포넌트와 store와 연결 후 store의 state를 꺼내, 필요한걸 props로 넣어주는 역할.
+  const todos = useSelector((state) => state.todos);
+  return <TodoList todos={todos} />;
+}
 
 export default TodoListContainer;
